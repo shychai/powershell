5e70e413cafa5df113a90cf3ef16b494ba2d1e64
shychai <raj.brayana@gmail.com>
2026-06-29 16:24:09 -0700

    extract downloads archives script

--- Workspace.ps1
+++ Workspace.ps1
@@ -1,2 +1,8 @@
 . .\Globals.ps1
 . .\Methods.ps1
+
+# ----------------------------------------
+# Extract Downloads Archives
+# ----------------------------------------
+Extract-Archives $Paths.Downloads
+Remove-FilesByFormat $Paths.Downloads $Formats.Archives
